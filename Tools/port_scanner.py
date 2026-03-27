1309a51c15e002be1a0f267f3f3ad7a2912f00bd
Sabaly99 <naniasabaly99@gmail.com>
2026-03-27 21:30:18 +0000

    update to the code to accept domain names and multiple targets scan

--- Tools/port_scanner.py
+++ Tools/port_scanner.py
@@ -1,14 +1,51 @@
 import socket
 from IPy import IP
 
+def domain_name_to_ip(ip_addr):
+    try :
+        IP(ip_addr)
+        return(ip_addr)
+    except ValueError:
+        try:
+            return socket.gethostbyname(ip_addr)
+        except socket.gaierror:
+            return None
 
-ipaddress = input("[+] Enter Target to Scan: ")
+ipaddress = input("[+] Enter Target to Scan: ").strip()
+Actual_IP = domain_name_to_ip(ipaddress)
 port = int(input('[+]Enter Target Port: '))
 
-try:
-    sock = socket.socket()
-    sock.connect((ipaddress,port))
-    print(f"[+] Port {port} is OPEN")
+#first function
+def port_scan():
 
-except:
-    print(f"Port {port} is CLOSED")
+    try:
+        sock = socket.socket()
+        sock.connect((Actual_IP,port))
+        sock.settimeout(20)
+        print(f"[+] Port {port} is OPEN")
+
+    except:
+        print(f"[-]Port {port} is CLOSED")
+
+#second function
+def scan_anyways(varr):
+    try:
+        soc = socket.socket()
+        soc.connect((varr,port))
+        print(f"[+] Port {port} is OPEN")
+    
+    except:
+        print(f"[-]Port {port} is CLOSED")
+
+
+if ',' in ipaddress:
+    for A in ipaddress.split(','):
+        target = domain_name_to_ip(A.strip())
+        result = scan_anyways(target)
+        print(result)  
+
+else:
+    pass
+    
+
+print(port_scan())
